fefd519f8e25887330a79c751216bf4e8bb05d17
emredurrmus <emredurmus0@hotmail.com>
2023-04-27 19:27:36 +0300

    Initial Commit

---
+++ NewsPaper/NewsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPaperApp.swift
+//  NewsPaper
+//
+//  Created by Emre Durmuş on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
